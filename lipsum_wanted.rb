bdb1f7f91a1b349e2825fa64835d2ca02d9b34fd
CosmicCandi <kalea.wolff@gmail.com>
2017-01-30 13:14:12 -0500

    First working commit of lipsum_wanted.rb

---
+++ lipsum_wanted.rb
@@ -0,0 +1,33 @@
+lipsum_wanted = ARGV[0]
+
+puts "lipsum_wanted value is: #{lipsum_wanted}"
+
+lipsum1 = "Pansy. We're rescuing ya. Perhaps, but perhaps your civilization is
+merely the sewer of an even greater society above you! Throw her in the brig.
+Why would a robot need to drink? Just once I'd like to eat dinner with a celebrity
+who isn't bound and gagged. I'm Santa Claus! In your time, yes, but nowadays shut
+up! Besides, these are adult stemcells, harvested from perfectly healthy adults
+whom I killed for their stemcells. What are you hacking off? Is it my torso?!
+'It is! ' My precious torso!"
+
+lipsum2 = "And I'd do it again! And perhaps a third time! But that would be it.
+ We're also Santa Claus! Are you crazy? I can't swallow that. What are their names?
+ I was all of history's great robot actors - Acting Unit 0.8; Thespomat; David
+ Duchovny!"
+
+lipsum3 = "But I've never been to the moon! Incidentally, you have a dime up your
+  nose. I decline the title of Iron Cook and accept the lesser title of Zinc Saucier,
+  which I just made up. Uhh… also, comes with double prize money. Say what? I
+  could if you hadn't turned on the light and shut off my stereo. I saw you with
+  those two 'ladies of the evening' at Elzars. Explain that. You mean while I'm
+  sleeping in it? I am the man with no name, Zapp Brannigan!"
+
+if lipsum_wanted.to_i == 1
+  puts lipsum1
+elsif lipsum_wanted.to_i == 2
+  puts lipsum2
+elsif lipsum_wanted.to_i == 3
+  puts lipsum3
+else
+  puts "Please enter a number 1 to 3"
+end
